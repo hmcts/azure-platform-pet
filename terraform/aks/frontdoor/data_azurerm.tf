a41544c3d8e62df2633df5a8c93949e1c1c78fb6
mokainos <72137340+mokainos@users.noreply.github.com>
2020-12-03 09:54:30 +0000

    added provider

--- terraform/aks/frontdoor/data_azurerm.tf
+++ terraform/aks/frontdoor/data_azurerm.tf
@@ -3,3 +3,9 @@ data "azurerm_client_config" "current" {}
 data "azuread_group" "operations_group" {
   name = "DTS Operations (env:${var.env})"
 }
+data "azurerm_log_analytics_workspace" "log" {
+  provider = azurerm.loganalytics
+  name                = "hmcts-prod"
+  resource_group_name = "oms-automation"
+
+}
--- terraform/aks/frontdoor/role.tf
+++ terraform/aks/frontdoor/role.tf
@@ -1,10 +1,6 @@
-data "azurerm_client_config" "loganalytics" {
-  provider = azurerm.loganalytics
-}
-
 resource "azurerm_role_assignment" "role" {
   provider             = azurerm.loganalytics
-  scope                = data.azurerm_client_config.loganalytics.id
+  scope                = data.azurerm_log_analytics_workspace.log.id
   role_definition_name = "Log Analytics Contributor"
   principal_id         = data.azuread_group.operations_group.object_id
 
