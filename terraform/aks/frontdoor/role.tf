f463203fbc2b0654b78363b740e8edf40aed8f23
mokainos <72137340+mokainos@users.noreply.github.com>
2020-12-03 09:41:51 +0000

    added provider

--- terraform/aks/frontdoor/role.tf
+++ terraform/aks/frontdoor/role.tf
@@ -1,10 +1,10 @@
-data "azurerm_client_config" "current" {
+data "azurerm_client_config" "loganalytics" {
   provider = azurerm.loganalytics
 }
 
 resource "azurerm_role_assignment" "role" {
   provider             = azurerm.loganalytics
-  scope                = data.azurerm_client_config.current.id
+  scope                = data.azurerm_client_config.loganalytics.id
   role_definition_name = "Log Analytics Contributor"
   principal_id         = data.azuread_group.operations_group.object_id
 
