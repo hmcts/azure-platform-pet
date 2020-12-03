785551c7b90cf4d81d718c602e6f6052daceb2c0
mokainos <72137340+mokainos@users.noreply.github.com>
2020-12-03 09:38:43 +0000

    added provider

--- terraform/aks/frontdoor/00-init.tf
+++ terraform/aks/frontdoor/00-init.tf
@@ -19,6 +19,6 @@ provider "azurerm" {
 }
 provider "azurerm" {
   features {}
-  alias           = loganalytics
+  alias           = "loganalytics"
   subscription_id = "8999dec3-0104-4a27-94ee-6588559729d1"
 }
