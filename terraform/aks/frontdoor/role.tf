07a08a5c6d6543eb2dc16ad3502abed5e795aea3
mokainos <72137340+mokainos@users.noreply.github.com>
2020-12-03 09:20:26 +0000

    added

--- terraform/aks/frontdoor/role.tf
+++ terraform/aks/frontdoor/role.tf
@@ -1,5 +1,9 @@
- resource "azurerm_role_assignment" "role" {
-  scope                = "8999dec3-0104-4a27-94ee-6588559729d1"
+data "azurerm_subscription" "log" {
+  subscription_id = "8999dec3-0104-4a27-94ee-6588559729d1"
+} 
+
+resource "azurerm_role_assignment" "role" {
+  scope                =  data.azurerm_subscription.log.id 
   role_definition_name = "Log Analytics Contributor"
   principal_id         = data.azuread_group.operations_group.object_id
 
